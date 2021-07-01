90a13e43d9fac1c099677df1b24240efd53aea21
chechoid <sergio.garciamora@gmail.com>
2021-07-01 15:26:36 -0300

    Actualiza description y corrige errores en comentarios

---
+++ R/attach.R
@@ -0,0 +1,4 @@
+.onAttach <- function(...)
+{
+  packageStartupMessage(sprintf("pepaitba v.%s :)\nTutoriales de las clases de Pensamiento Estadístico en People Analytics \n / Diplomatura People Analytics del ITBA", packageVersion("pepaitba")))
+}
